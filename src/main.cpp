1e37aaf2ce20ae558d930606aebdaaa123084d8a
Romerico <mrh.jrd.234@gmail.com>
2025-11-11 12:57:30 -0500

    Added very basic code for user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <string>
+
 int main() {
+    while (true) {
+        std::string input;
+
+        std::cout << R"(
+Testing Custom Shell. Enter any of the following commands, or -1 to quit:
+- cd
+- rmdir
+- touch
+- cp
+- chown
+- ls / dir
+- echo
+- grep
+- help
+- pause
+- pwd
+- mkdir
+- rm
+- mv
+- chmod
+- cat
+- wc
+- environ
+- clr
+- quit
+)" << std::endl;
+
+        std::cout << "Enter command: ";
+        std::cin >> input;
+
+        if (input == "-1") {
+            std::cout << "Exiting shell..." << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
